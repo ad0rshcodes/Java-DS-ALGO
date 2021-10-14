0647f9d357322d28b5d201ee0b36ec7e18c8bdb6
adarsh-sharma6218 <adarshsharma6218@gmail.com>
2021-10-14 11:11:04 +0530

    test commit for IntelliJ

--- src/com/adarsh/Main.java
+++ src/com/adarsh/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+
+        int a = 100;
+
+        int b = a;
+
+        a = 80;
+
+        System.out.println(b);
+        System.out.println(a);
+
+
+
     }
 }
