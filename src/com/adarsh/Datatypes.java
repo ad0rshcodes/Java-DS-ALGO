1a44575c3c0dc5002623c43ce1f65630c10d8829
adarsh-sharma6218 <adarshsharma6218@gmail.com>
2021-11-18 00:44:32 +0530

    feat: starting the lecture series again

--- src/com/adarsh/Datatypes.java
+++ src/com/adarsh/Datatypes.java
@@ -11,8 +11,8 @@ public class Datatypes {
 //        Primitive datatype: a datatype that can't broken further int, char, etc. can be broken while String can be broken down into further characters.
 
         int age = 16; //creating a int variable
-
-        Integer marks = new Integer(56); //method to create an Int object. This allow us to perform a lot of functions on the integer.
+        int marks = 50;
+        // Integer marks = new Integer(56); method to create an Int object. This allow us to perform a lot of functions on the integer.
 
         System.out.println(marks);
         //Functions can be accessed by writing "marks."
